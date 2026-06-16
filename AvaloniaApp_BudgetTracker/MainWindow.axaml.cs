bdf8bf8436c643b31e8bdf57331d9ce9071655fb
Devrim Aslan <devrim.aslan@bulme.at>
2026-06-16 12:19:15 +0200

    Initial commit

---
+++ AvaloniaApp_BudgetTracker/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaApp_BudgetTracker
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
